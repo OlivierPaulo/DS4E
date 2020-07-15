20947d9d31874cc696904262493a14d73c02e3de
Olivier <op.olivierpaulo@gmail.com>
2020-07-15 14:53:56 +0200

    Adding Lists folder

---
+++ le-wagon-exercises/lists/portfolio.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-docstring
+
+aapl = [ 10, 154.12 ]
+goog = [  2, 812.56 ]
+tsla = [ 12, 342.12 ]
+fb   = [ 18, 209.0  ]
+
+portfolio = [ aapl, goog, tsla, fb ]
+
+#print(portfolio[3])
+#            AAPL     GOOG    TSLA      FB
+market  = [ 198.84, 1217.93, 267.66, 179.06 ]
+pnl = 0
+for i in range(len(portfolio)):
+    pnl += portfolio[i][0]*(market[i]-portfolio[i][1])
+    
+print(pnl)
