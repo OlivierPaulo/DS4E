1574e27711d528b0f6f3cd4c0671afdf6b4add4a
Reivil0 <reivil0@pop-os.localdomain>
2020-07-15 12:15:23 +0200

    adding financials Python files

---
+++ le-wagon-exercises/financials/financials.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-docstring
+import math
+
+# TODO: 1st exercise: Define the `forward_price` function
+def forward_price(spot, interest_rate, time):
+    return round(spot * math.exp(interest_rate*time),2)
+
+# TODO: 2nd exercise: Define the `short_pnl` function
+def short_pnl(positions, mtm):
+    pnl = 0
+    for i in range(len(positions)):
+        pnl += positions[i] - mtm[i]
+    return pnl
+        
