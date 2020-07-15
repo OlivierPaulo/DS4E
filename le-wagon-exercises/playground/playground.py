b13292d79ee0cc74c56a11b91cd3c002404e68ea
Olivier <op.olivierpaulo@gmail.com>
2020-07-15 13:30:51 +0200

    Playground modified with function

--- le-wagon-exercises/playground/playground.py
+++ le-wagon-exercises/playground/playground.py
@@ -1,14 +1,15 @@
 # TODO: it's a playground, let's write some code (no unit tests to run here)
 import math
 
+def circle_math(radius):
+    perimeter = round(radius * 2 * math.pi,1)
+    area = round(radius**2 * math.pi,1)
+    return [perimeter,area]
 
 print ("Please choose the radius of a circle : ")
 radius = int(input())
-perimeter = round(radius * 2 * math.pi,1)
 
-print(f"Radius of the circle is : {radius}")
-print(f"Perimeter of the circle is : {perimeter}")
+values = circle_math(radius)
+print(f"Radius={radius} => Perimeter={values[0]}, Area={values[1]}")
 
-area = round(radius**2 * math.pi,1)
-print(f"Area of the circle is : {area}")
 
